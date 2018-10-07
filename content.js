fbe6ea337b953a22c34bf8fe2eb1a0d4760750a0
aaking <aaking@zynga.com>
2018-10-06 17:46:42 -0700

    Use presets

--- content.js
+++ content.js
@@ -1,3 +1,8 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 var data = {};
 
 data["pri"] = {
@@ -7,4 +12,4 @@ data["pri"] = {
     description: "The Kidnapping of Princess Pri is an exploration/adventure/escape game. Foil (and maybe carry out) a kidnapping by searching items, talking to characters, finding keys, and solving clues. Playtime under an hour and the whole thing is pretty light-hearted."
 };
 
-export default data;
+exports.default = data;
